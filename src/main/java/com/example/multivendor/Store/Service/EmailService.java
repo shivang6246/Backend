69b93bb464218f027a03020c64bc213921c4277c
Shivang <your-github-email@gmail.com>
2026-02-18 22:29:09 +0530

    email

--- src/main/java/com/example/multivendor/Store/Service/EmailService.java
+++ src/main/java/com/example/multivendor/Store/Service/EmailService.java
@@ -1,23 +1,34 @@
 package com.example.multivendor.Store.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 
     private final JavaMailSender mailSender;
 
     public void sendOtpEmail(String toEmail, String otp) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Your OTP Code");
-        message.setText("Your OTP is: " + otp + "\nIt will expire in 5 minutes.");
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("ultronhor037@gmail.com"); // MUST match verified sender
+            message.setTo(toEmail);
+            message.setSubject("Your OTP Code");
+            message.setText("Your OTP is: " + otp + "\nIt will expire in 5 minutes.");
 
-        mailSender.send(message);
+            mailSender.send(message);
+
+            log.info("Email successfully sent to {}", toEmail);
+
+        } catch (Exception e) {
+            log.error("Email sending failed: {}", e.getMessage(), e);
+            throw new RuntimeException("Email sending failed");
+        }
     }
 }
